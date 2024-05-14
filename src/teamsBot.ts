766f2de65fb2f2cb01046150359404126833812e
Selvi-Parasakthi-K <kjparasakthi562003@gmail.com>
2024-05-14 11:19:33 +0530

    created payload

--- src/teamsBot.ts
+++ src/teamsBot.ts
@@ -1,9 +1,46 @@
-import { TeamsActivityHandler } from "botbuilder";
+import { TeamsActivityHandler, TurnContext } from "botbuilder";
 
-// An empty teams activity handler.
-// You can add your customization code here to extend your bot logic if needed.
 export class TeamsBot extends TeamsActivityHandler {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+    }
+
+    async handleMessage(context: TurnContext) {
+        const message = context.activity.text;
+        const entities = context.activity.entities;
+        const sender = context.activity.from.name;
+        const sender_id = context.activity.from.id;
+        let points, recipient, recipient_id, reason;
+        
+        entities.forEach(entity => {
+            if (entity.type === "mention" && entity.mentioned) {
+                if (entity.mentioned.name != sender) {
+                    recipient = entity.mentioned.name;
+                    recipient_id = entity.mentioned.id;
+                }
+            }
+        });
+
+        const parts = message.split(" "); 
+        const indexFor = parts.indexOf("for");
+        if (indexFor !== -1) {
+            points = parseInt(parts[indexFor - 1]);
+            reason = parts.slice(indexFor + 1).join(" "); 
+        }
+
+        const parsedData = {
+            points: points,
+            sender: sender,
+            recipient: recipient,
+            reason: reason,
+            sender_id: sender_id,
+            recipient_id: recipient_id
+        };
+
+        console.log(parsedData);
+    }
+
+    async onMessageActivity(context: TurnContext) {
+        await this.handleMessage(context);
+    }
 }
